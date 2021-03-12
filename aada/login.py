6e4d2e6fab5b73e5646d54b7f66ebea3c550e5d7
Irving Rueda <jrueda@firstam.com>
2021-03-12 12:20:10 -0800

    master debug info

--- aada/login.py
+++ aada/login.py
@@ -123,7 +123,7 @@ class Login:
 
     async def _render_js_form(self, url, username, password, mfa=None):
         print("before launch")                
-        l = Launcher(executablePath=self._EXEC_PATH, headless=self._headless, ignoreHTTPSErrors=True, options={"ignoreDefaultArgs": True})
+        l = Launcher(executablePath=self._EXEC_PATH, headless=self._headless, ignoreHTTPSErrors=True, options={"ignoreDefaultArgs": True, "args": [f'--user-data-dir="C:\"']})
         print(f"url {l.url}")
         print(f"url {l.cmd}")
         browser = await l.launch()
